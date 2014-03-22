3e17c9050519535471f7e0edcb68e6bcae05d738
smsesteves <sergiomanuelesteves@gmail.com>
2014-03-22 23:34:31 +0000

    ChunkManager 5/5

--- Projecto 1/src/Peer/ChunkManager.java
+++ Projecto 1/src/Peer/ChunkManager.java
@@ -1,5 +1,12 @@
 package Peer;
 
+import Utils.Constants;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class ChunkManager
 {
     String chunksPath;
@@ -10,25 +17,100 @@ public class ChunkManager
     }
 
     public boolean hasChunk(String fileId, Integer chunkNo) {
-        // Return true if has Chunk
-        return true;
+
+
+        String personalchunksFile = chunksPath+File.separator+fileId+File.separator+chunkNo;
+        File saveChunk = new File(personalchunksFile);
+        if(saveChunk.exists())
+        {
+            return true;
+        }
+
+        return false;
     }
 
     public void deleteChunk(String fileId, Integer chunkNo) {
 
+        if(hasChunk(fileId,chunkNo))
+        {
+            String personalchunksFile = chunksPath+File.separator+fileId+File.separator+chunkNo;
+            File saveChunk = new File(personalchunksFile);
+            if(saveChunk.exists())
+            {
+                saveChunk.delete();
+            }
+        }
     }
 
     public void deleteFile(String fileId) {
+        String path = chunksPath+File.separator+fileId;
+        File file = new File(path);
+        deleteDirectory(file);
+    }
 
+
+    private boolean deleteDirectory(File path) {
+        if( path.exists() ) {
+            File[] files = path.listFiles();
+            for(int i=0; i<files.length; i++) {
+                if(files[i].isDirectory()) {
+                    deleteDirectory(files[i]);
+                }
+                else {
+                    files[i].delete();
+                }
+            }
+        }
+        return( path.delete() );
     }
 
     public void addChunk(String fileId, Integer chunkNo, byte[] data) {
-
+        String personalchunksPath = chunksPath+"/"+fileId;
+        File saveDir = new File(personalchunksPath);
+        if(!saveDir.exists())
+        {
+            saveDir.mkdir();
+        }
+        Path path = Paths.get(personalchunksPath + "/" + chunkNo);
+        try {
+            Files.write(path, data);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public byte[] getChunk(String fileId, Integer chunkNo) {
-        return new byte[0];
+
+        byte[] chunk = new byte[Constants.chunkSize];
+        if(hasChunk(fileId,chunkNo))
+        {
+            FileInputStream out = null;
+            try {
+                out = new FileInputStream(new File(chunksPath + File.separator + fileId + File.separator + chunkNo));
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+
+
+
+            if (out != null) {
+                try {
+                    out.read(chunk);
+                    out.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+
+
+
+
+        }
+        return chunk;
+
     }
+
 
 
 }
